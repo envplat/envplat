38efb17a2aaab9c7e1584463e51310e394a96428
xencodes <xencodes@icloud.com>
2022-04-25 21:29:27 +0530

    add project fetch api using access

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -2,9 +2,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import shortid from 'shortid';
 import prisma from 'providers/prisma';
+import { Project } from '@prisma/client';
 
 type Data = {
 	ok: boolean;
+	project?: Project | null;
 };
 
 export default async function handler(
@@ -23,6 +25,24 @@ export default async function handler(
 
 		res.status(200).json({ ok: true });
 		return;
+	}
+
+	if (req.method === 'GET') {
+		const { access } = req.query;
+		if (!access) {
+			return res.status(400).json({ ok: false });
+		}
+
+		const project = await prisma.project.findFirst({
+			where: {
+				access: access as string,
+			},
+			include: {
+				envs: true,
+			},
+		});
+
+		return res.status(200).json({ ok: true, project });
 	}
 
 	res.status(200).json({ ok: false });
